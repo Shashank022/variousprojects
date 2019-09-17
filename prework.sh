bc10ed4fa8373bf67399b56d354ee860a96aa9eb
Shashank022 <shashank022@gmail.com>
2019-09-16 22:59:37 -0400

    Added chnages

--- prework.sh
+++ prework.sh
@@ -1,3 +1,3 @@
 sudo docker build -t spothakanoori/vuejsproj .
 
-sudo docker run -it -p 8080:80 --rm --name spothakanoori/vuejsproj spothakanoori/vuejsproj:latest
+sudo docker run -i -p 8080:80 --rm --name spothakanoori/vuejsproj spothakanoori/vuejsproj:latest
