6727425c8b6a202bc7a6c6f8173ded4e5f4494b8
Shashank022 <shashank022@gmail.com>
2019-09-19 23:52:37 -0400

    Added new Tab as Git Hub

---
+++ src/views/GitHub.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card text-center m-3">
+        <h3 class="card-header">Vue.js Pagination Tutorial & Example</h3>
+        <div class="card-body">
+        </div>
+        <div class="card-footer pb-0 pt-3">
+        </div>
+    </div>
+</template>
+
+<script>
+// an example array of items to be paged
+export default {
+    data() {
+        return {
+        };
+    },
+    methods: {
+        onChangePage(pageOfItems) {
+            // update page of items
+            this.pageOfItems = pageOfItems;
+        }
+    }
+};
+</script>
