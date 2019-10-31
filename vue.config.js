1b15cb81b2905fba7767dc91414a5d15551ba584
Shashank022 <shashank022@gmail.com>
2019-10-30 23:41:19 -0400

    Addede new changes

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+module.exports = {
+  chainWebpack: config => {
+    if (process.env.VUE_SERVE_DOCS) {
+      config.entry('app' ).set(path.resolve(__dirname, './docs/main.js')
+    }
+  }
+}
